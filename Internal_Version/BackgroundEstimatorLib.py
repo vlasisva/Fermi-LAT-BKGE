14f766c041ad13e0dd0675acbadc5ab2a1575c6a
Giacomo Vianello <giacomov@slac.stanford.edu>
2013-12-05 17:30:41 -0800

    Changed soft links with hard links, so git will upload the actual file

--- Internal_Version/BackgroundEstimatorLib.py
+++ Internal_Version/BackgroundEstimatorLib.py
@@ -1,1 +1,19 @@
-/home/giacomov/software/myBKGE/GRBAnalysis-scons/BackgroundEstimator/BackgroundEstimatorLib.py
+#$Id: BackgroundEstimatorLib.py,v 1.3 2011/10/06 10:41:50 vlasisva Exp $
+def generate(env, **kw):
+    if not kw.get('depsOnly',0):
+        env.Tool('addLibrary', library = ['BackgroundEstimator'], package = 'BackgroundEstimator')
+        pass
+    env.Tool('astroLib')
+    env.Tool('st_facilitiesLib')
+    #env.Tool('LikelihoodLib')
+    env.Tool('irfInterfaceLib')
+    env.Tool('astroLib')
+    env.Tool('irfLoaderLib')
+    env.Tool('rootIrfLoaderLib')
+    env.Tool('addLibrary', library=env['clhepLibs'])
+    env.Tool('addLibrary', library=env['rootLibs'])
+    #env.Tool('addLibrary', library=env['STforGRBLibs'])
+    pass
+
+def exists(env):
+    return 1
